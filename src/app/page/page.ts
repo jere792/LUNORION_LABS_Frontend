4c1c8ae3597dad0771245a7f22d4c934604e5c5d
jere792 <138648408+jere792@users.noreply.github.com>
2026-06-04 23:16:28 -0500

    [04/06/2026] FEAT: MEJORAS 1.1

---
+++ src/app/page/page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page',
+  imports: [],
+  template: `
+    <p>
+      page works!
+
+      ng generate component page
+    </p>
+  `,
+  styles: ``,
+})
+export class Page {
+
+}
